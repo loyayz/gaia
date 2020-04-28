912395eeb793c4f8a553c4cf568d47390e233486
loyayz <loyayz@foxmail.com>
2020-04-28 10:59:33 +0800

    feat(data): mybatis-plus 新增修改时自动插入时间默认值

--- gaia-data/src/main/java/com/loyayz/gaia/data/autoconfigure/GaiaMybatisPlusAutoConfiguration.java
+++ gaia-data/src/main/java/com/loyayz/gaia/data/autoconfigure/GaiaMybatisPlusAutoConfiguration.java
@@ -1,16 +1,21 @@
 package com.loyayz.gaia.data.autoconfigure;
 
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.loyayz.gaia.data.mybatis.AbstractEntity;
 import com.loyayz.gaia.data.mybatis.extension.DefaultSqlInjector;
 import com.loyayz.gaia.util.Sequence;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
 
 /**
  * @author loyayz (loyayz@foxmail.com)
@@ -44,6 +49,32 @@ public class GaiaMybatisPlusAutoConfiguration {
                 return sequence.nextId();
             }
 
+        };
+    }
+
+    @Bean
+    @ConditionalOnClass(MetaObjectHandler.class)
+    @ConditionalOnMissingBean(MetaObjectHandler.class)
+    public MetaObjectHandler metaObjectHandler() {
+        return new MetaObjectHandler() {
+            @Override
+            public void insertFill(MetaObject metaObject) {
+                Object originalObject = metaObject.getOriginalObject();
+                if (originalObject instanceof AbstractEntity) {
+                    AbstractEntity entity = (AbstractEntity) originalObject;
+                    Date current = new Date();
+                    entity.setGmtCreate(current);
+                    entity.setGmtModified(current);
+                }
+            }
+
+            @Override
+            public void updateFill(MetaObject metaObject) {
+                Object originalObject = metaObject.getOriginalObject();
+                if (originalObject instanceof AbstractEntity) {
+                    ((AbstractEntity) originalObject).setGmtModified(new Date());
+                }
+            }
         };
     }
 
---
+++ gaia-data/src/main/java/com/loyayz/gaia/data/mybatis/AbstractEntity.java
@@ -0,0 +1,46 @@
+package com.loyayz.gaia.data.mybatis;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.util.Date;
+
+/**
+ * @author loyayz (loyayz@foxmail.com)
+ */
+public abstract class AbstractEntity<T extends BaseEntity> extends BaseEntity<T> {
+
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+}
