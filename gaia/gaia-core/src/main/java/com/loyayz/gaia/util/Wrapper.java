89806f2d7985dee8a762f5e882358b4b940a81a6
loyayz <loyayz@foxmail.com>
2020-05-12 01:29:06 +0800

    feat(gaia): [core] 空函数容器

---
+++ gaia/gaia-core/src/main/java/com/loyayz/gaia/util/Wrapper.java
@@ -0,0 +1,14 @@
+package com.loyayz.gaia.util;
+
+/**
+ * @author loyayz (loyayz@foxmail.com)
+ */
+@FunctionalInterface
+public interface Wrapper {
+
+    /**
+     * 执行
+     */
+    void execute();
+
+}
