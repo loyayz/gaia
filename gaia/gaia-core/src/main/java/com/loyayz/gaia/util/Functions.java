a9157bc243657fb3490c15735b8b73e44307e98d
loyayz <loyayz@foxmail.com>
2020-07-08 20:46:50 +0800

    feat(gaia): [core] Functions

--- gaia/gaia-core/src/main/java/com/loyayz/gaia/util/Functions.java
+++ gaia/gaia-core/src/main/java/com/loyayz/gaia/util/Functions.java
@@ -1,6 +1,10 @@
 package com.loyayz.gaia.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author loyayz (loyayz@foxmail.com)
@@ -15,6 +19,26 @@ public final class Functions {
         if (obj != null) {
             consumer.accept(obj);
         }
+    }
+
+    /**
+     * 转换 list
+     */
+    public static <T, R> List<R> convert(List<T> origin, Function<T, R> mapper) {
+        if (origin == null) {
+            return new ArrayList<>();
+        }
+        return origin.stream().map(mapper).collect(Collectors.toList());
+    }
+
+    /**
+     * 转换 bean
+     */
+    public static <T, R> R convert(T origin, Function<T, R> mapper) {
+        if (origin == null) {
+            return null;
+        }
+        return mapper.apply(origin);
     }
 
 }
